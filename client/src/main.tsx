dc9d9f431e33a55376890f957023ea94e4d14094
Vidhman <130959953+Vidhman@users.noreply.github.com>
2025-10-06 16:27:38 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Tell wouter to use your repo’s subpath (/StudentProfile/)
+const base = import.meta.env.BASE_URL.replace(/\/$/, "");
+
+createRoot(document.getElementById("root")!).render(
+  <Router base={base}>
+    <App />
+  </Router>
+);
